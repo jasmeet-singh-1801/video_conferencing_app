fc02810523d20fc5a02151ada7e1bb5df03cca2a
jasmeet-singh-1801 <jasmeetsingh2872@gmail.com>
2024-05-09 21:41:13 +0530

    Logo Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+            logoImageUrl: "/icons/logo.svg",
           },
           variables: {
             colorText: "#fff",
